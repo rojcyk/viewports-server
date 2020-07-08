8c0f1f14bfe14ba53f02dcbb7f2c12bc89022f9f
Denis Rojcyk <rojcyk@users.noreply.github.com>
2020-07-08 13:19:01 +0200

    Removing more commented out code

--- src/models/Display.ts
+++ src/models/Display.ts
@@ -1,8 +1,6 @@
 import {Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany } from "typeorm"
 
 import Viewport from '@models/Viewport'
-// import User from '@models/user/model'
-// import Role from '@models/role/model'
 
 @Entity()
 export class Display {
@@ -23,15 +21,6 @@ export class Display {
 
   @OneToMany(type => Viewport, viewport => viewport.platform)
   viewport?: Viewport
-
-  // @OneToMany(type => Viewport, viewport => viewport.platform)
-  // viewport?: Viewport
-
-  // @ManyToOne(type => Team, team => team.members)
-  // team!: Team
-
-  // @ManyToOne(type => Role)
-  // role!: Role
 }
 
 export default Display
