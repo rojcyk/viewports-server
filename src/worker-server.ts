7ca3477e19cdfc2448dd8d2bcb8e40c29ed9f75b
Denis Rojcyk <rojcyk@users.noreply.github.com>
2020-07-08 13:16:18 +0200

    Adjusted the names of imports

--- src/worker-server.ts
+++ src/worker-server.ts
@@ -2,11 +2,11 @@ import 'reflect-metadata'
 
 import app from './app'
 import {createConnection, ConnectionOptions } from 'typeorm'
-import dbConfigJs from '../ormconfig'
+import dbConfig from '../ormconfig'
 
 const PORT = process.env.PORT || '8000'
 
-createConnection(dbConfigJs as ConnectionOptions).then(connection => {
+createConnection(dbConfig as ConnectionOptions).then(connection => {
   console.log(`[server] Local enviroment: ${process.env.NODE_ENV}`)
   console.log(`[server] Connected to the database: ${process.env.DB_NAME}`)
 
