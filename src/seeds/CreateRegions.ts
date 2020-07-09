d89e82a9a4089d618ae2928411654f119bb193e0
Denis Rojcyk <rojcyk@users.noreply.github.com>
2020-07-09 22:24:08 +0200

    Adjusted code spacing

--- src/seeds/CreateRegions.ts
+++ src/seeds/CreateRegions.ts
@@ -9,35 +9,35 @@ export default class CreateRegions implements Seeder {
       .insert()
       .into(Region)
       .values([
-          {
-            title: 'Worldwide',
-            code: 'ww',
-            url: 'Worldwide',
-          }, {
-            title: 'Africa',
-            code: 'af',
-            url: 'Africa',
-          }, {
-            title: 'Asia',
-            code: 'as',
-            url: 'Asia',
-          }, {
-            title: 'Europe',
-            code: 'eu',
-            url: 'Europe',
-          }, {
-            title: 'Oceania',
-            code: 'oc',
-            url: 'Oceania',
-          }, {
-            title: 'North America',
-            code: 'na',
-            url: 'North%20America',
-          }, {
-            title: 'South America',
-            code: 'sa',
-            url: 'South%20America',
-          }
+        {
+          title: 'Worldwide',
+          code: 'ww',
+          url: 'Worldwide',
+        }, {
+          title: 'Africa',
+          code: 'af',
+          url: 'Africa',
+        }, {
+          title: 'Asia',
+          code: 'as',
+          url: 'Asia',
+        }, {
+          title: 'Europe',
+          code: 'eu',
+          url: 'Europe',
+        }, {
+          title: 'Oceania',
+          code: 'oc',
+          url: 'Oceania',
+        }, {
+          title: 'North America',
+          code: 'na',
+          url: 'North%20America',
+        }, {
+          title: 'South America',
+          code: 'sa',
+          url: 'South%20America',
+        }
       ])
       .execute()
   }
