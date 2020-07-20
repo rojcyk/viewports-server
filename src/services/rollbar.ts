316f533177db89081e6da737f9257f039e95ecd4
rojcyk <denis@rojcyk.com>
2020-07-20 23:08:20 +0200

    Removing access token (because I'm a dumbass)

--- src/services/rollbar.ts
+++ src/services/rollbar.ts
@@ -1,7 +1,7 @@
 import Rollbar from 'rollbar'
 
 const rollbar = new Rollbar({
-  accessToken: 'b3e91c74202549aaa38870fe935b5e5d',
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
   environment: process.env.NODE_ENV
