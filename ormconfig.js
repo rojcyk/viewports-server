f79db73c4faf70d9edfcfd6631e224ebe80ebca3
rojcyk <denis@rojcyk.com>
2020-07-20 23:28:01 +0200

    Moving over to database url

--- ormconfig.js
+++ ormconfig.js
@@ -8,11 +8,12 @@ const migrations = path.join(__dirname, `${folderPath}/migrations`, `*.{ts,js}`)
 
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST || '',
-  port: process.env.DB_PORT || 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
+  // host: process.env.DB_HOST || '',
+  // port: process.env.DB_PORT || 5432,
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD || '',
+  // database: process.env.DB_NAME,
   entities: [entities],
   migrations: [migrations],
   migrationsRun: true,
