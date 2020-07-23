689bd4269aa80b2e6701270d59629c8bc701786b
rojcyk <denis@rojcyk.com>
2020-07-24 01:58:42 +0200

    Fixing typescript errors

--- src/helpers/statCounter/index.ts
+++ src/helpers/statCounter/index.ts
@@ -12,13 +12,23 @@ export default async (
 
   let regionRepository = getManager().getRepository(Region)
   const allRegions = await regionRepository.find()
-  const platformData: StatCounter.PlatformData = {}
+  const platformData: {
+    [K in StatCounter.RegionCode]: StatCounter.Result[]
+  } = {
+    ww: [],
+    af: [],
+    as: [],
+    eu: [],
+    oc: [],
+    na: [],
+    sa: []
+  }
 
   await asyncForEach(allRegions, async (region: Region) => {
       console.log(`- Downloading ${platformName} for ${region.title} ...`)
       const csvPlatformData = await downloadDataForRegion(platformName, region)
       const processedPlatformData = await parseCSV(csvPlatformData)
-      platformData[region.code] = processedPlatformData
+      platformData[region.code as StatCounter.RegionCode] = processedPlatformData
   })
 
   return platformData
